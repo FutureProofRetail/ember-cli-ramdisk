0748de38ef460dd1dfd5fe1a9e09f7b9b5f1c235
machty <machty@gmail.com>
2014-10-31 12:26:23 -0400

    Use project root rather than cwd

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var fs = require('fs');
 var execSync = require('execSync').exec;
 var rimraf = require('rimraf');
 
-var TMP_PATH = "./tmp";
 var RAMDISK_BYTES = 2 * 1024 * 1024 * 1024;
 var BLOCK_SIZE = 512;
 var RAMDISK_BLOCKS = RAMDISK_BYTES / BLOCK_SIZE;
@@ -25,11 +24,11 @@ function runCommand(command) {
   return result.stdout;
 }
 
-function removeOldTmpDirectory() {
-  if (fs.readdirSync(TMP_PATH).length > 0) {
-    throw new Error("Your " + TMP_PATH + " directory isn't empty. Please empty it or remove it so that ember-cli-ramdisk can take its place.");
+function removeOldTmpDirectory(projectTmpPath) {
+  if (fs.readdirSync(projectTmpPath).length > 0) {
+    throw new Error("Your " + projectTmpPath + " directory isn't empty. Please empty it or remove it so that ember-cli-ramdisk can take its place.");
   }
-  rimraf.sync(TMP_PATH);
+  rimraf.sync(projectTmpPath);
 }
 
 function createRamdiskDevice() {
@@ -47,39 +46,42 @@ function createRamdiskIfNecessary() {
 
   var devicePath = createRamdiskDevice();
   mountRamdiskDevice(devicePath);
-
 }
 
-function createSymlink(projectName) {
+function createSymlink(projectTmpPath, projectName) {
   var ramdiskTmpPath = RAMDISK_PATH + "/" + projectName;
 
   if (!fs.existsSync(ramdiskTmpPath)) {
     fs.mkdirSync(ramdiskTmpPath);
   }
 
-  fs.symlinkSync(ramdiskTmpPath, TMP_PATH, 'dir');
+  fs.symlinkSync(ramdiskTmpPath, projectTmpPath, 'dir');
 }
 
 module.exports = {
   name: 'ember-cli-ramdisk',
   included: function(app) {
+
+    var projectTmpPath = app.project.root + "/tmp";
+
     if (process.platform !== 'darwin') {
       console.log("ember-cli-ramdisk presently only supports Mac. No ramdisk will be installed.")
       return;
     }
 
-    if (fs.existsSync(TMP_PATH)) {
-      if (fs.lstatSync(TMP_PATH).isSymbolicLink()) {
-        if (fs.readlinkSync(TMP_PATH).indexOf(RAMDISK_PATH) === -1) {
+    if (fs.existsSync(projectTmpPath)) {
+      if (fs.lstatSync(projectTmpPath).isSymbolicLink()) {
+        if (fs.readlinkSync(projectTmpPath).indexOf(RAMDISK_PATH) === -1) {
           throw new Error("It seems like you've already symlinked your tmp directory elsewhere; please rm it and try again.");
         }
         return;
       }
-      removeOldTmpDirectory();
+      removeOldTmpDirectory(projectTmpPath);
     }
 
     createRamdiskIfNecessary();
-    createSymlink(app.project.pkg.name);
-    console.log("ember-cli-ramdisk: your ramdisk has been created! Enjoy your speedy builds.");
+    createSymlink(projectTmpPath, app.project.pkg.name);
+    console.log("ember-cli-ramdisk: your ramdisk has been created at " + RAMDISK_PATH + ". Enjoy your speedy builds.");
   }
 };
+
